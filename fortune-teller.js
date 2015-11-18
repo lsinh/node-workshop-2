aa288dc4c90be7c1b653b064b368dfe8d1cccff9
lsinh <leichin.sinha@gmail.com>
2015-11-18 22:34:04 +0000

    completed server.js exercise

---
+++ fortune-teller.js
@@ -0,0 +1,9 @@
+
+//Accessing module located in library folder, in a file called fortune.js
+var fortune = require('./library/fortune');
+
+
+//calling fortune for first time
+//console.log(fortune.getFortune());
+
+var numberofRequests = process.argv[2];
